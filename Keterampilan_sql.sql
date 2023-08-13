a57fa4531589a3a754e1f229fc1d2b7c88e1f300
Faridwm <fwmubarok@gmail.com>
2023-08-13 08:23:59 +0700

    1) Pertama kali komit. 2) Menyelesaikan unjuk keterampilan dengan bahasa pemrograman python dan sql. 3) commit dataset awal dan dataset yang sudah dibersihkan

---
+++ Keterampilan_sql.sql
@@ -0,0 +1,37 @@
+select
+	make,
+	count(make) as total_jenis_mobil
+from
+	automobile
+group by
+	make
+order by
+	total_jenis_mobil desc;
+
+select
+	`fuel-type`,
+	count(`fuel-type`) as total_jenis_bahan_bakar
+from
+	automobile
+group by
+	`fuel-type`
+order by
+	total_jenis_bahan_bakar desc;
+
+select
+	make,
+	horsepower
+from
+	automobile
+order by
+	horsepower desc
+limit 5;
+
+select
+	*
+from
+	automobile
+where
+	`fuel-type` = "gas"
+	and `body-style` = "sedan";
+
